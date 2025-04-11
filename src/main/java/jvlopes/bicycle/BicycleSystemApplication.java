ff8c23728cbce3ee4a4a43a6450098675bbe1985
Joao Vitor Lopes <94joaovitorls@gmail.com>
2025-04-10 22:57:48 -0300

    chore: add spring init project

---
+++ src/main/java/jvlopes/bicycle/BicycleSystemApplication.java
@@ -0,0 +1,13 @@
+package jvlopes.bicycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicycleSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicycleSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jvlopes/bicycle/BicycleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package jvlopes.bicycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicycleSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
