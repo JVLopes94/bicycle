86ddb8971765a9ec5bea67120b7458d53439df65
Joao Vitor Lopes <94joaovitorls@gmail.com>
2025-04-10 23:58:46 -0300

    feat: add bicycle repository

---
+++ src/main/java/jvlopes/bicycle/fleet/domain/repository/BicycleRepository.java
@@ -0,0 +1,9 @@
+package jvlopes.bicycle.fleet.domain.repository;
+
+import jvlopes.bicycle.fleet.domain.entity.Bicycle;
+
+public interface BicycleRepository {
+
+    public Bicycle save(Bicycle bicycle);
+
+}
