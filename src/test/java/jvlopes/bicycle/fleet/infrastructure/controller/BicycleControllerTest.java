03f582a004acfdc6a8cea6fdd64fb10877c32742
Joao Vitor Lopes <94joaovitorls@gmail.com>
2025-04-18 21:36:28 -0300

    test: add failing test for bicycle create location header

--- src/test/java/jvlopes/bicycle/fleet/infrastructure/controller/BicycleControllerTest.java
+++ src/test/java/jvlopes/bicycle/fleet/infrastructure/controller/BicycleControllerTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -63,7 +64,21 @@ class BicycleControllerTest {
 
         @Test
         void shouldReturnLocationHeader() {
-
+            String bicycleId = "113e-c296-9048";
+            Bicycle createdBicycle = BicycleTestFactory.createBicycleWithID(new BicycleID(bicycleId));
+            doReturn(createdBicycle).when(bicycleService).save(any());
+            ResponseEntity<BicycleCreatedDTO> response = bicycleController.create(new CreateBicycleDTO(
+                    bicycleId,
+                    "model",
+                    "AVAILABLE",
+                    "SP",
+                    "2025-04-10T10:15:30"
+            ));
+            HttpHeaders responseHeaders = response.getHeaders();
+            assertNotNull(responseHeaders);
+            assertTrue(responseHeaders.containsKey("Location"));
+            assertNotNull(responseHeaders.getLocation());
+            assertTrue(responseHeaders.getLocation().toString().contains(bicycleId));
         }
     }
 }
